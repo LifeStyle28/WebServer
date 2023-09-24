325778b2b4575650f43300c2929038d95deb3710
Monweal <elaldermon495@gmail.com>
2023-09-24 16:50:23 +0500

    script adaptation

--- application/use_cases.cpp
+++ application/use_cases.cpp
@@ -9,6 +9,8 @@ namespace app
 
 namespace fs = std::filesystem;
 
+#define DOCS_TAR "docs.tar"
+
 BringTagValuesError::BringTagValuesError(Reason reason) :
     std::runtime_error{"Failed to bring tag values"},
     m_reason{reason}
@@ -42,32 +44,86 @@ CreateResultFileUseCase::CreateResultFileUseCase(fs::path scriptPath, fs::path r
 {
 }
 
-//@ TODO завернуть в strand и сделать через m_resultPath и m_scriptPath
-//@ TODO разделить на несколько функций/методов
-static void start_script(const std::string& savePath, const std::string& jsonConfig,
-    const std::string& scriptPath)
+/**
+ * @brief      Makes a directory.
+ *
+ * @param[in]  savePath  The save path
+ */
+static void make_dir(const std::string& savePath)
 {
-    /// создание папки для результатов
     std::stringstream command;
     command << "mkdir -p ";
     command << savePath;
-    std::system(command.str().c_str()); // @TODO unused result?
+    if (std::system(command.str().c_str()) != 0)
+    {
+        throw std::runtime_error{"Failed to create folder"};
+    }
+}
 
-    /// вызов скрипта
-    command.str("");
+/**
+ * @brief      Исполняет python-скрипт
+ *
+ * @param[in]  savePath    The save path
+ * @param[in]  jsonConfig  The json configuration
+ * @param[in]  scriptPath  The script path
+ */
+static void call_script(const std::string& savePath, const std::string& jsonConfig,
+    const std::string& scriptPath)
+{
+    std::stringstream command;
     command << "python3 ";
     command << '\'' << scriptPath << '\'' << ' '; ///< script path
     command << '\'' << jsonConfig << '\'' << ' '; ///< json-string
     command << '\'' << savePath << '\''; ///< path for save
-    std::system(command.str().c_str());
+    if (std::system(command.str().c_str()) != 0)
+    {
+        throw std::runtime_error{"Failed to make documents with python-script"};
+    }
+}
 
-    /// упаковка документов в архив
-    command.str("");
+/**
+ * @brief      Removes an old archive.
+ *
+ * @param[in]  savePath  The save path
+ */
+static void remove_old_archive(const std::string& savePath)
+{
+    std::stringstream command;
+    command << "rm -rf ";
+    command << savePath;
+    command << DOCS_TAR;
+    std::system(command.str().c_str());
+}
+
+/**
+ * @brief      Makes an archive.
+ *
+ * @param[in]  savePath  The save path
+ */
+static void make_archive(const std::string& savePath)
+{
+    std::stringstream command;
     command << "tar -cvf ";
     command << savePath;
-    command << "docs.tar "; // @TODO - сделать алиас, повторяется больше, чем в 1 месте
+    command << DOCS_TAR;
+    command << " -C ";
     command << savePath;
-    std::system(command.str().c_str());
+    command << ".";
+    if (std::system(command.str().c_str()) != 0)
+    {
+        throw std::runtime_error{"Failed to create tar"};
+    }
+}
+
+//@ TODO завернуть в strand
+static void start_script(const std::string& savePath, const std::string& jsonConfig,
+    const std::string& scriptPath)
+{
+    std::stringstream command; ///< @TODO REMOVE
+    make_dir(savePath); ///< создает папку для результатов
+    call_script(savePath, jsonConfig, scriptPath); ///< вызывает скрипт
+    remove_old_archive(savePath); ///< удаляет старый архив, если он есть
+    make_archive(savePath); ///< упаковывает документы в архив
 }
 
 std::string CreateResultFileUseCase::CreateFile(const std::string& body) const
@@ -77,8 +133,8 @@ std::string CreateResultFileUseCase::CreateFile(const std::string& body) const
         std::mt19937_64 randomizer{};
         const std::string folderName(std::to_string(randomizer())); ///< название сгенерированной папки
         const std::string generatedFolderPath(static_cast<std::string>(m_resultPath) + folderName + "/"); ///< полный путь сгенерированной папки
-        const std::string path = generatedFolderPath + "/docs.tar";
-        start_script(generatedFolderPath, body, m_scriptPath);
+        const std::string path = generatedFolderPath + '/' + DOCS_TAR;
+        start_script(generatedFolderPath, body, m_scriptPath); ///< @TODO проверить пути и работу скрипта
         return path;
     }
     catch (const std::exception& e)
--- script/script.py
+++ script/script.py
@@ -3,27 +3,20 @@ import sys
 import re
 import json
 import os
+import logging
 
 from docxcompose.composer import Composer
 from docx import Document as Document_compose
 from num_to_rus import Converter
-# from docx2pdf import convert
 
 import datetime
 from dateutil.relativedelta import *
 
-# argv[1] - путь до шаблона json
+# argv[1] - строка шаблона json
 # argv[2] - путь для сохранения файла
 
 def space_num(num):
     return format(num, ',d').replace(',',' ')
-
-def combine_all_docx(filename_master, merge_file):
-    master = Document_compose(filename_master)
-    composer = Composer(master)
-    doc_temp = Document_compose(merge_file)
-    composer.append(doc_temp)
-    composer.save("combined_file.docx")
 
 def paragraph_replace_text(paragraph, regex, replace_str):
     while True:
@@ -69,23 +62,23 @@ def fill_cell_date(table, column_num, use_date, row_set = 1):
                 cur_date = use_date.strftime("%d.%m.%Y")
                 value.paragraphs[0].runs[0].text = cur_date
 
-def make_tables(document, type, loan_sum, perc, date):
-        print("table type with num=", type)
+def make_tables(document, type, tag_dict):
+        logging.debug(f'table type with num={type}')
         if type <= 4:
-                make_table_1(document, loan_sum, perc, date)
+                make_table_1(document, tag_dict)
         elif type <= 9:
-                make_table_2(document, loan_sum, perc, date)
+                make_table_2(document, tag_dict)
         elif type <= 10:
-                make_table_3(document, loan_sum, perc, date, 'долларов США')
+                make_table_3(document, tag_dict, 'долларов США')
         elif type <= 11:
-                make_table_3(document, loan_sum, perc, date, 'Евро')
+                make_table_3(document, tag_dict, 'Евро')
 
-def make_table_1(document, loan_sum, perc, date):
-        print("make_table_1")
-        conv = Converter()
-        date = datetime.datetime.strptime(date, '%d.%m.%Y')
+def make_table_1(document, tag_dict):
+        logging.debug('make_table_1')
+        date = datetime.datetime.strptime(tag_dict['@<DATE>@'], '%d.%m.%Y')
+        loan_sum = int(tag_dict['@<SUMM_NUMBER>@'].replace(' ', ''))
 
-        payment = loan_sum * perc // 1200
+        payment = loan_sum * int(tag_dict['@<PERCENT_NUMBER>@']) // 1200
 
         hold_sum = payment * 13 // 100
         if (payment * 13) % 100 > 0:
@@ -93,60 +86,60 @@ def make_table_1(document, loan_sum, perc, date):
 
         result_sum = payment - hold_sum
 
-        #Проверка на нужную таблицу
+        # проверка на нужную таблицу
         for table in document.tables:
                 if table.rows[0].cells[0].text  == '№ п/п':
-                        fill_cell_date(table, 1, date) # Дата погашения
-                        fill_cell_table(table, 2, space_num(payment)) # Сумма платежа
-                        fill_cell_table(table, 3, space_num(hold_sum)) # Удерживаемая сумма налога
-                        fill_cell_table(table, 4, space_num(result_sum)) # Итоговая сумма выплаты Займодавцу
-                        table.columns[5].cells[13].paragraphs[0].runs[0].text = space_num(loan_sum) + ' (' + str(conv.convert(loan_sum)) + ') рублей' # Сумма основного долга
+                        fill_cell_date(table, 1, date) # дата погашения
+                        fill_cell_table(table, 2, space_num(payment)) # сумма платежа
+                        fill_cell_table(table, 3, space_num(hold_sum)) # удерживаемая сумма налога
+                        fill_cell_table(table, 4, space_num(result_sum)) # итоговая сумма выплаты Займодавцу
+                        table.columns[5].cells[13].paragraphs[0].runs[0].text = space_num(loan_sum) + ' (' + str(Converter().convert(loan_sum)) + ') рублей' # сумма основного долга
 
                         # Заполнение ИТОГО
-                        table.rows[14].cells[2].paragraphs[0].runs[0].text = space_num(payment * 12) + ' (' + str(conv.convert(payment * 12)) + ') рублей' # Сумма платежа
-                        table.rows[14].cells[3].paragraphs[0].runs[0].text = space_num(hold_sum * 12) + ' (' + str(conv.convert(hold_sum * 12)) + ') рублей' # Удерживаемая сумма налога
-                        table.rows[14].cells[4].paragraphs[0].runs[0].text = space_num(result_sum * 12) + ' (' + str(conv.convert(result_sum * 12)) + ') рублей' # Итоговая сумма выплаты Займодавцу
-                        table.rows[14].cells[5].paragraphs[0].runs[0].text = space_num(loan_sum) + ' (' + str(conv.convert(loan_sum)) + ') рублей' # Сумма основного долга
+                        table.rows[14].cells[2].paragraphs[0].runs[0].text = space_num(payment * 12) + ' (' + str(Converter().convert(payment * 12)) + ') рублей' # сумма платежа
+                        table.rows[14].cells[3].paragraphs[0].runs[0].text = space_num(hold_sum * 12) + ' (' + str(Converter().convert(hold_sum * 12)) + ') рублей' # удерживаемая сумма налога
+                        table.rows[14].cells[4].paragraphs[0].runs[0].text = space_num(result_sum * 12) + ' (' + str(Converter().convert(result_sum * 12)) + ') рублей' # утоговая сумма выплаты Займодавцу
+                        table.rows[14].cells[5].paragraphs[0].runs[0].text = space_num(loan_sum) + ' (' + str(Converter().convert(loan_sum)) + ') рублей' # сумма основного долга
 
-def make_table_2(document, loan_sum, perc, date):
-        print("make_table_2")
-        conv = Converter()
-        date = datetime.datetime.strptime(date, '%d.%m.%Y')
+def make_table_2(document, tag_dict):
+        logging.debug('make_table_2')
+        date = datetime.datetime.strptime(tag_dict['@<DATE>@'], '%d.%m.%Y')
+        loan_sum = int(tag_dict['@<SUMM_NUMBER>@'].replace(' ', ''))
 
-        payment = loan_sum * perc // 1200
+        payment = loan_sum * int(tag_dict['@<PERCENT_NUMBER>@']) // 1200
 
-        #Проверка на нужную таблицу
+        # проверка на нужную таблицу
         for table in document.tables:
                 if table.rows[0].cells[0].text  == '№ п/п':
                         table.rows[0].cells[2].paragraphs[0].runs[0].text = 'Сумма процентов: ' + str(perc) + '%'
 
-                        fill_cell_date(table, 1, date) # Дата погашения
-                        fill_cell_table(table, 2, space_num(payment)) # Сумма платежа
+                        fill_cell_date(table, 1, date) # дата погашения
+                        fill_cell_table(table, 2, space_num(payment)) # сумма платежа
 
-                        table.columns[3].cells[13].paragraphs[0].runs[0].text = space_num(loan_sum) # Сумма основного долга
+                        table.columns[3].cells[13].paragraphs[0].runs[0].text = space_num(loan_sum) # сумма основного долга
 
                         # Заполнение ИТОГО
-                        table.rows[14].cells[2].paragraphs[0].runs[0].text = space_num(payment * 12) + ' (' + str(conv.convert(payment * 12)) + ') рублей' # Сумма платежа
-                        table.rows[14].cells[3].paragraphs[0].runs[0].text = space_num(loan_sum) + ' (' + str(conv.convert(loan_sum)) + ') рублей' # Сумма основного долга
+                        table.rows[14].cells[2].paragraphs[0].runs[0].text = space_num(payment * 12) + ' (' + str(Converter().convert(payment * 12)) + ') рублей' # сумма платежа
+                        table.rows[14].cells[3].paragraphs[0].runs[0].text = space_num(loan_sum) + ' (' + str(Converter().convert(loan_sum)) + ') рублей' # сумма основного долга
 
-def make_table_3(document, loan_sum, perc, date, currency):
-        print("make_table_3")
-        conv = Converter()
-        date = datetime.datetime.strptime(date, '%d.%m.%Y')
+def make_table_3(document, tag_dict, currency):
+        logging.debug('make_table_3')
+        date = datetime.datetime.strptime(tag_dict['@<DATE>@'], '%d.%m.%Y')
+        loan_sum = int(tag_dict['@<SUMM_NUMBER>@'].replace(' ', ''))
 
-        payment = loan_sum * perc // 1200
+        payment = loan_sum * int(tag_dict['@<PERCENT_NUMBER>@']) // 1200
 
-        #Проверка на нужную таблицу
+        # проверка на нужную таблицу
         for table in document.tables:
                 if table.rows[0].cells[0].text == '№':
 
-                        fill_cell_date(table, 1, date, 0) # Дата погашения
-                        fill_cell_table(table, 2, space_num(payment) + ' ' + currency + ' в российских рублях по курсу ЦБ РФ на день выплаты', 0) # Сумма платежа
+                        fill_cell_date(table, 1, date, 0) # дата погашения
+                        fill_cell_table(table, 2, space_num(payment) + ' ' + currency + ' в российских рублях по курсу ЦБ РФ на день выплаты', 0) # сумма платежа
 
-                        # Заполнение ИТОГО
-                        table.rows[12].cells[2].paragraphs[0].runs[0].text = 'Возвращается Сумма займа в размере ' + space_num(payment * 12) + ' (' + str(conv.convert(payment * 12)) + ') ' + currency # Сумма платежа
+                        # заполнение ИТОГО
+                        table.rows[12].cells[2].paragraphs[0].runs[0].text = 'Возвращается Сумма займа в размере ' + space_num(payment * 12) + ' (' + str(Converter().convert(payment * 12)) + ') ' + currency # сумма платежа
 
-def get_docs_nums(data): # Получим номера нужных документов
+def get_docs_nums(data): # получим номера нужных документов
         doc_nums = []
         for need_docs in data["docs"]:
                 doc_nums.append(int(need_docs))
@@ -183,39 +176,71 @@ def get_name_by_num(num):
                   23: 'Расписка (Euro)'}
     return file_names[num]
 
-if __name__ == "__main__":
-        print("Args length:", len(sys.argv))
-        print("Arg[1] =", str(sys.argv[1]))
-        print("Arg[2] =", str(sys.argv[2]))
+def correct_values(tag_dict):
+        if '@<SUMM_NUMBER>@' in tag_dict:
+                tag_dict['@<SUMM_NUMBER>@'] = space_num(int(tag_dict['@<SUMM_NUMBER>@'])) # если это сумма договора, то разделяем пробелами
 
-        error_status = 0
-        data = json.loads(str(sys.argv[1]))
-        data = data["contracts"][0]
+def replace_tags(document, tag_dict):
+        for tag, value in tag_dict.items(): # бежим по тэгам
+                regex = re.compile(tag)
+                for paragraph in document.paragraphs:
+                        paragraph_replace_text(paragraph, regex, value)
+                tables_replace_text(document, regex, value)
 
-        doc_nums = get_docs_nums(data)
+def make_fio_short(fio_full):
+        return fio_full.split()[0] + ' ' + fio_full.split()[1][0] + '. ' + fio_full.split()[2][0] + '.'
 
-        for i in doc_nums:
-                doc_num = int(i)
-                document = docx.Document("/app/templates/docx/" + str(doc_num) + ".docx")
+def split_passport(seria_num):
+        if len(seria_num.split()) == 1:
+                return seria_num[0:4], seria_num[4:]
+        return seria_num.split()[0], seria_num.split()[1]
 
-                for entry in data["tag_values"]:
-                        for curr in entry:
-                                if curr == "tag":
-                                        regex = re.compile(entry[curr])
-                                        if entry[curr] == "@<SUMM_NUMBER>@":
-                                            new_value = space_num(int(new_value)) # если это сумма договора, то разделяем пробелами
-                                elif curr == "key":
-                                        new_value = entry[curr]
-                        for paragraph in document.paragraphs:
-                                paragraph_replace_text(paragraph, regex, new_value)
-                        tables_replace_text(document, regex, new_value)
+def make_contract_num(tag_dict):
+        date = datetime.datetime.strptime(tag_dict['@<DATE>@'], '%d.%m.%Y')
+        datestr = date.strftime('%d%m%y')
+        return datestr + tag_dict['@<FIO_FULL>@'].split()[0][0] + tag_dict['@<FIO_FULL>@'].split()[1][0] + tag_dict['@<FIO_FULL>@'].split()[2][0]
 
-                loan_sum, perc, date = get_table_need_values(data)
-                make_tables(document, doc_num, loan_sum, perc, date)
+def add_extra_tags(tag_dict):
+        if '@<SUMM_NUMBER>@' in tag_dict:
+                tag_dict['@<SUMM_TEXT>@'] = str(Converter().convert(int(tag_dict['@<SUMM_NUMBER>@'])))
+        if '@<PERCENT_NUMBER>@' in tag_dict:
+                tag_dict['@<PERCENT_TEXT>@'] = str(Converter().convert(int(tag_dict['@<PERCENT_NUMBER>@'])))
+        if '@<FIO_FULL>@' in tag_dict:
+                tag_dict['@<FIO_SHORT>@'] = make_fio_short(tag_dict['@<FIO_FULL>@'])
+        if '@<PASSPORT_SERIA_NUM>@' in tag_dict:
+                tag_dict['@<PASSPORT_SERIA>@'], tag_dict['@<PASSPORT_NUM>@'] = split_passport(tag_dict['@<PASSPORT_SERIA_NUM>@'])
+        tag_dict['@<GUARANTEE_NUM>@'] = str(make_contract_num(tag_dict))
+        tag_dict['@<CONTRACT_NUM>@'] = str(make_contract_num(tag_dict))
 
-                save_name = str(sys.argv[2] + get_name_by_num(doc_num) + ".docx")
-                print("Name to save is", save_name)
+def make_fio_date(tag_dict):
+        return tag_dict['@<DATE>@'] + ' ' + tag_dict['@<FIO_SHORT>@']
+
+def make_docs():
+        data = json.loads(str(sys.argv[1])) # загружаем шаблон
+        data = data['contracts'][0] # встаём на единственный контракт
+
+        tag_dict = {} # создаем словарь соответствия тэгов и значений
+        for entry in data['tag_values']: # заполняем словарь тэгов
+                tag_dict[entry['tag']] = str(entry['key'])
+        add_extra_tags(tag_dict) # добавить новые необходимые теги, такие как текстовые представления чисел
+        correct_values(tag_dict) # подмениваем значения по необходимости
+        logging.debug(f'tag_dict = : {tag_dict}')
+
+        doc_nums = get_docs_nums(data) # получаем номера документов
+        for doc_num in doc_nums: # бежим по номерам документов
+                document = docx.Document(DOCX_TEMPLATES_PATH + str(doc_num) + '.docx') # открываем шаблон документа
+                replace_tags(document, tag_dict) # заменяем тэги на нужные значения
+                make_tables(document, doc_num, tag_dict) # заполняем таблицы
+                save_name = str(sys.argv[2] + get_name_by_num(doc_num) + ' от ' + make_fio_date(tag_dict) + ".docx")
+                logging.debug(f"Name to save is {save_name}")
                 document.save(save_name)
-        print("Program exit. Error status is", error_status)
-        if error_status == 1:
-            exit(int(error_status))
+
+if __name__ == "__main__":
+        LOG_FULL_NAME = 'python_script.log'
+        DOCX_TEMPLATES_PATH = '/app/templates/docx/'
+
+        logging.basicConfig(level=logging.DEBUG, filename=LOG_FULL_NAME) # запись логов уровня дебаг в файл LOG_FULL_NAME
+        logging.debug(f'Args length: {len(sys.argv)}')
+        logging.debug(f'Args = {str(sys.argv)}')
+
+        make_docs()
