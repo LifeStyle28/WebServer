80441f4db1b860a6f025c3348af45283c3157833
Маховик Никита Сергеевич <n.mahovik@prosoftsystems.ru>
2023-11-06 21:00:51 +0500

    docker-compose

--- cron/dir_remover.py
+++ cron/dir_remover.py
@@ -2,16 +2,20 @@
 import re
 import subprocess
 import os
+import logging
 from datetime import datetime
 from datetime import timedelta
 
-for file in os.listdir(r'/app/web/result'):
-	command = r'stat /app/web/result/' + file + r' | grep Access:\ 2'
+logging.basicConfig(level=logging.DEBUG, filename='/cron/dir_remover.log')
+
+for file in os.listdir(r'/result'):
+	command = r'stat /result/' + file + r' | grep Access:\ 2'
 	time_str = subprocess.getoutput(command)
 	match = re.search(r'(\d\d\d\d-\d\d-\d\d) (\d\d:\d\d:\d\d)', time_str)
 	last_use_datetime = datetime.fromisoformat(str(match[1]) + ' ' + str(match[2]));
 
-	if (datetime.today() - last_use_datetime) > timedelta(hours=1):
-		print('rm -rf ' + file)
+	if (datetime.today() - last_use_datetime) > timedelta(minutes=30):
+		logging.debug(f'rm -rf /result/{file}')
+		os.system('rm -rf /result/' + file)
 	else:
-		print(file, ' left')
+		logging.debug(f'{file} left')
